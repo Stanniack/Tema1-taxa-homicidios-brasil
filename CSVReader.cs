ea74131dc7332d22b3554a7a56109f793770e6da
Mateus Vitor <44790156+Stanniack@users.noreply.github.com>
2020-09-25 23:18:21 -0300

    Initial commit

---
+++ CSVReader.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tema1
+{
+    class CSVReader
+    {
+        private const string Path = @"C:\Users\Mateus\source\repos\Tema1\homicidios-geral.csv";
+
+        public ArrayList Reader()
+        {
+            String[] data = null;
+            String result = null;
+            ArrayList list = new ArrayList();
+
+            try
+            {
+                StreamReader reader = new StreamReader(Path, Encoding.UTF8, true);
+
+                while (reader.ReadLine() != null)
+                {
+                    data = reader.ReadLine().Split(';');
+
+                    list.Add(data);
+                }
+
+            } catch (FileNotFoundException e)
+            {
+                Console.WriteLine("Erro na procura do arquivo: " + e);
+            }
+
+
+            return list;
+            
+        }
+    }
+
+}
+
+
+
+
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tema1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            foreach(String[] t in new CSVReader().Reader())
+            {
+                Console.WriteLine(t[3]);
+            }
+
+        }
+    }
+}
